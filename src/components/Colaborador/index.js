c55671827951ea98735f60b9ce3e4900dfb08ee7
gutolive09 <gutolive01@gmail.com>
2023-07-21 13:54:18 -0300

    projeto finalizado.

--- src/components/Colaborador/index.js
+++ src/components/Colaborador/index.js
@@ -1,14 +1,35 @@
+import {AiFillCloseCircle, AiFillHeart, AiOutlineHeart} from 'react-icons/ai';
 import './Colaborador.css'
 
-const Colaborador = ({nome, imagem, cargo, corDeFundo}) => {
+const Colaborador = ({nome, imagem, cargo, corDeFundo, id, aoDeletar, aoFavoritar}) => {
+    function favoritar() {
+        aoFavoritar(id)
+    }
+
+    const propsFavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
     return(
         <div className='colaborador'>
+            <AiFillCloseCircle 
+                size={25} 
+                className='deletar' 
+                onClick={() => aoDeletar(id)}
+            />
             <div className='cabecalho' style={{backgroundColor: corDeFundo}}>
                 <img src={imagem} alt={nome} />
             </div>
             <div className='rodape'>
                 <h4>{nome}</h4>
                 <h5>{cargo}</h5>
+                <div className='favoritar'>
+                    {Colaborador.favorito 
+                        ? <AiFillHeart {...propsFavorito} color='#ff0000'/> 
+                        : <AiOutlineHeart {...propsFavorito}/>
+                    }
+                </div>
             </div>
         </div>
     )
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import Botao from '../Botao'
-import CampoTexto from '../CampoTexto'
+import Campo from '../Campo'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
+import { v4 as uuidv4 } from 'uuid';
 
 const Formulario = (props) => {
 
@@ -10,6 +11,9 @@ const Formulario = (props) => {
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
+    const [id, setId] = useState('')
+    const [nomeTime, setNomeTime] = useState('')
+    const [corTime, setCorTime] = useState('')
 
     const aoSalvar = (evento)=> {
         evento.preventDefault()
@@ -17,47 +21,79 @@ const Formulario = (props) => {
             nome,
             cargo,
             imagem,
-            time
+            time,
+            id
         })
         setNome('')
         setCargo('')
         setImagem('')
         setTime('')
+        setId(uuidv4())
+        console.log(id)
     }
 
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto 
+                <Campo
+                    type='text' 
                     obrigatorio={true} 
                     label='Nome' 
                     placeholder="Digite o seu nome"
                     valor={nome}
                     aoAlterado={valor => setNome(valor)}
-                    />
-                <CampoTexto 
+                />
+                <Campo
+                    type='text' 
                     obrigatorio={true} 
                     label='Cargo' 
                     placeholder="Digite o seu cargo"
                     valor={cargo}
                     aoAlterado={cargo => setCargo(cargo)}
-                    />
-                <CampoTexto 
+                />
+                <Campo
+                    type='text' 
                     label='Imagem' 
                     placeholder="Digite o endereço da imagem"
                     valor={imagem}
                     aoAlterado={imagem => setImagem(imagem)}
-                    />
-                <ListaSuspensa 
+                />
+                <ListaSuspensa
+                    type='text' 
                     obrigatorio={true} 
                     label='Time' 
                     itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
-                    />
+                />
                 <Botao>
                     Criar Card
+                </Botao>
+            </form>
+            <form onSubmit={(evento) => {
+                evento.preventDefault()
+                props.cadastrarTime({nome: nomeTime, cor: corTime})
+            }}>
+                <h2>Preencha os dados para criar um novo time.</h2>
+                <Campo
+                    type='text' 
+                    obrigatorio
+                    label='Nome' 
+                    placeholder="Digite o nome do time"
+                    valor={nomeTime}
+                    aoAlterado={valor => setNomeTime(valor)}
+                />
+                <Campo
+                    type='color' 
+                    obrigatorio 
+                    label='Cor' 
+                    placeholder="Digite a cor do time"
+                    valor={corTime}
+                    aoAlterado={cargo => setCorTime(cargo)}
+                />
+                <Botao>
+                    Criar time
                 </Botao>
             </form>
         </section>
